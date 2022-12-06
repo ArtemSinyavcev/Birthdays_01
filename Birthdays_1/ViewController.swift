275a4865b0aa570970e4c4d356b964bde4ec49c0
Artem Sinyavcev <artem.sinyavcev.gmail.com>
2022-12-06 15:33:15 +0500

    Initial Commit

---
+++ Birthdays_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Birthdays_1
+//
+//  Created by Артём Синявцев on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
