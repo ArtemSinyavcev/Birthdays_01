e0d7c5198ea52362e07d4cda3dc27fe19464fb4c
Artem Sinyavcev <artem.sinyavcev.gmail.com>
2022-12-07 18:29:11 +0500

    Добавил Table View, строку поиска, кривую номерацию...

---
+++ Birthdays_1/OneTableCell.swift
@@ -0,0 +1,31 @@
+//
+//  MailTableCell.swift
+//  Birthdays_1
+//
+//  Created by Артём Синявцев on 07.12.2022.
+//
+
+import UIKit
+
+class OneTableCell: UITableViewCell {
+
+  
+    
+    @IBOutlet weak var dataTable: UILabel!
+    @IBOutlet weak var nameTable: UILabel!
+    @IBOutlet weak var imageTable: UIImageView!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Birthdays_1/ViewController.swift
+++ Birthdays_1/ViewController.swift
@@ -9,14 +9,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let idTable = "iDTable"
+    var poisk = UISearchController()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
+        tableView.dataSource = self
+        tableView.delegate = self
         
         
-        //  Created by Артём Синявцев on 06.12.2022.
+            // добавление БОЛЬШОГО Navigation Bar
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+            // добавление СТРОКИ ПОИСКА, расширяем класс через extension(ниже) и создаем переменную (выше)
+        poisk = UISearchController(searchResultsController: nil)
+        poisk.searchResultsUpdater = self
+        self.navigationItem.searchController = poisk
+        
     }
 
 
 }
 
+
+
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: idTable) as! OneTableCell
+        cell.dataTable.text = "17 января 1991 г."
+        cell.nameTable.text = "Синявцев Артем"
+        cell.imageTable.image = UIImage(named: "smoke1")
+        cell.imageTable.layer.cornerRadius = 40
+        cell.textLabel?.text = String(indexPath.item)
+        
+        return cell
+    }
+    
+             // ВЫСОТА ЯЧЕЙКИ (heightForRowAt)
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
+   
+    
+}
+
+
+
+
+extension ViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+         
+    }
+
+}
