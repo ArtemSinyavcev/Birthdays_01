2b45eaf7a35757fdb4afd3a23b3074ab5754dafe
Artem Siniavtsev <artem.sinyavcev@gmail.com>
2022-12-08 23:25:02 +0500

    добавил новый класс и аутлеты

---
+++ Birthdays_1/TwoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TwoViewController.swift
+//  Birthdays_1
+//
+//  Created by Артём Синявцев on 08.12.2022.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    @IBOutlet weak var dataRozhdeniyaText: UITextField!
+    @IBOutlet weak var familiyaText: UITextField!
+    @IBOutlet weak var imageOneView: UIImageView!
+    @IBOutlet weak var samButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+    
+    @IBAction func addButton(_ sender: UIButton) {
+        print("Лёнич")
+    }
+    
+    
+    
+}
