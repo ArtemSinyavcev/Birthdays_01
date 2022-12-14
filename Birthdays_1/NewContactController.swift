b1899b849afaffaff7dfdd8247408db72e58f48d
Artem Siniavtsev <artem.sinyavcev@gmail.com>
2022-12-14 10:33:16 +0500

    Исправил отступы, переименовал оутлеты , экшен, файл.

---
+++ Birthdays_1/NewContactController.swift
@@ -0,0 +1,30 @@
+//
+//  TwoViewController.swift
+//  Birthdays_1
+//
+//  Created by Артём Синявцев on 08.12.2022.
+//
+
+import UIKit
+
+class TwoViewController: UIViewController {
+
+    @IBOutlet weak var dataTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var imageAddImageView: UIImageView!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var surnameNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButtonAction(_ sender: UIButton) {
+        
+        let text = nameTextField.text
+        surnameNameLabel.text = text
+       
+    }
+    
+}
--- Birthdays_1/OneTableCell.swift
+++ Birthdays_1/OneTableCell.swift
@@ -9,34 +9,21 @@ import UIKit
 
 class OneTableCell: UITableViewCell {
 
-  
-    
     @IBOutlet weak var dataTable: UILabel!
     @IBOutlet weak var nameTable: UILabel!
     @IBOutlet weak var imageTable: UIImageView!
     
-    
-    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+       
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
 
 
-// Configure the view for the selected state
-// 5
-// 6
-// 7
-// 8
-// 99
-// 100
-// 1000
-// 11000
+
